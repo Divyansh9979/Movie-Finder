5016826cae138b0934e3aeb25edbfe91ac07b6af
Divyansh Mittal <64131961+Divyansh9979@users.noreply.github.com>
2025-01-16 13:54:01 +0530

    added maven configs and dependencies

---
+++ src/test/java/com/example/Movie/Finder/MovieFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movie.Finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
